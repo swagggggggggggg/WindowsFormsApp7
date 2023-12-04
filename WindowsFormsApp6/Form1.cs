88fde77e5f9c921c450415578de49e2b4f0be099
Egor <Egor@DESKTOP-GC3R1TS>
2023-12-04 08:30:30 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+
+        private int rand_num;
+        private int win_rate = 0;
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+   
+        private void pictureBox1_Click(object sender, EventArgs e) 
+        {
+            rand_num = random.Next(1, 4);
+     
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            
+
+            pictureBox5.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\1(2).jpg");
+
+            if (rand_num == 1)
+            {
+                label3.ForeColor = Color.Red;
+                label3.Text = "LOSE!";
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(9).jpg");
+            }
+
+            if (rand_num == 2)
+            {
+                label3.ForeColor = Color.Green;
+                label3.Text = "WIN!";
+                win_rate++;
+                textBox1.Text = Convert.ToString(win_rate);
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(10).jpg");
+            }
+
+            if (rand_num == 3)
+            {
+                label3.ForeColor = Color.Black;
+                label3.Text = "DRAW!";
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(8).jpg");
+            }
+            
+        }
+
+
+
+            
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(win_rate);
+        }
+      
+        private void pictureBox2_Click(object sender, EventArgs e) 
+        {
+            rand_num = random.Next(1, 4);
+            pictureBox5.Image = null;
+            pictureBox7.Image = null;
+            
+
+            pictureBox6.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\1(2).jpg");
+
+            if (rand_num == 1)
+            {
+                label3.ForeColor = Color.Black;
+                label3.Text = "DRAW!";
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(9).jpg");
+            }
+
+            if (rand_num == 2)
+            {
+                label3.ForeColor = Color.Red;
+                label3.Text = "LOSE!";
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(10).jpg");
+            }
+
+            if (rand_num == 3)
+            {
+                label3.ForeColor = Color.Green;
+                label3.Text = "WIN!";
+                win_rate++;
+                textBox1.Text = Convert.ToString(win_rate);
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(8).jpg");
+            }
+        }
+ 
+        private void pictureBox3_Click(object sender, EventArgs e) 
+        {
+            rand_num = random.Next(1, 4);
+            pictureBox6.Image = null;
+            pictureBox5.Image = null;
+            
+
+            pictureBox7.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\1(2).jpg");
+
+            if (rand_num == 1)
+            {
+                label3.ForeColor = Color.Green;
+                label3.Text = "WIN!";
+                win_rate++;
+                textBox1.Text = Convert.ToString(win_rate);
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(9).jpg");
+            }
+
+            if (rand_num == 2)
+            {
+                label3.ForeColor = Color.Black;
+                label3.Text = "DRAW!";
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(10).jpg");
+            }
+
+            if (rand_num == 3)
+            {
+                label3.ForeColor = Color.Red;
+                label3.Text = "LOSE!";
+                pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\Новый проект(8).jpg");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            win_rate = 0;
+            textBox1.Text = Convert.ToString(win_rate);
+            label3.Text = "";
+            pictureBox4.Image = Image.FromFile("C:\\Users\\Egor\\Downloads\\1(1).jpg");
+            pictureBox6.Image = null;
+            pictureBox5.Image = null;
+            pictureBox7.Image = null;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
